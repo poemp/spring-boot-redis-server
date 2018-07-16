e0a449d219cea162f28b8f55dabe06d4b3315506
lywworking <luyanwen2010>
2018-07-16 10:24:00 +0800

    first commit

---
+++ src/main/java/org/poem/SpringBootServerServerApplication.java
@@ -0,0 +1,12 @@
+package org.poem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootServerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootServerServerApplication.class, args);
+	}
+}
